27ac99c021618ec860449cecc2dbf2532e3aae11
rohanpatil3890-a11y <rohanpatil3890@gmail.com>
2025-12-29 16:41:16 +0530

    css update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { ShopcardComponent } from './shopcard/shopcard.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopcardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shopcard/shopcard.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShopcardComponent } from './shopcard.component';
+
+describe('ShopcardComponent', () => {
+  let component: ShopcardComponent;
+  let fixture: ComponentFixture<ShopcardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShopcardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShopcardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
